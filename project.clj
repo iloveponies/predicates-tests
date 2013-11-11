720d5dbc63d1cd6360dd20829f1c597501b7c989
Jani Rahkola <jani.rahkola@iki.fi>
2013-11-11 11:23:51 +0200

    init

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject iloveponies.tests/predicates "0.1.0-SNAPSHOT"
+  :description "tests for predicates"
+  :url "http://github.com/iloveponies/predicates"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [midje "1.5.1"]
+                 [midje-grader "0.1.0-SNAPSHOT"]])
